dcbe950c53d894d474b5b65fb4d76af789e114ee
Sven Nilsen <bvssvni@gmail.com>
2015-10-28 23:26:53 +0100

    Read meta data for entities
    - Prepare for converting
    - Updated to latest piston_meta
    - Fixed “memory_requirements” example
    - Added `data::entities::load`
    - Use same order of rules in entity syntax as document
    - Added optional name component

--- examples/memory_requirements.rs
+++ examples/memory_requirements.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let world = size_of::<World>();
     // init, prev, current, next.
-    let physics = 4 * size_of::<[Vec3; ENTITY_COUNT]>();
+    let physics = 4 * size_of::<[Vec3; world::ENTITY_COUNT]>();
     let sum = world + physics;
     println!("Memory requirements {} MiB", sum as f64 / 1024.0 / 1024.0);
 }
