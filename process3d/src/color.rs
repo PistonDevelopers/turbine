755c057583cd1a4df0637e78590903650188d6ef
Sven Nilsen <bvssvni@gmail.com>
2025-11-22 16:03:21 +0100

    Added alpha blending

--- process3d/src/color.rs
+++ process3d/src/color.rs
@@ -69,3 +69,46 @@ pub fn rgba_gamma_linear_to_srgb(c: Rgba) -> Rgba {
         c[3],
     ]
 }
+
+/// Converts color to `f32` precision.
+pub fn rgba_to_f32(a: Rgba<u8>) -> Rgba {
+    [
+        (a[0] as f32) / 255.0,
+        (a[1] as f32) / 255.0,
+        (a[2] as f32) / 255.0,
+        (a[3] as f32) / 255.0,
+    ]
+}
+
+/// Converts color to `u8` precision.
+pub fn rgba_to_u8(a: Rgba) -> Rgba<u8> {
+    [
+        (a[0] * 255.0) as u8,
+        (a[1] * 255.0) as u8,
+        (a[2] * 255.0) as u8,
+        (a[3] * 255.0) as u8,
+    ]
+}
+
+/// Alpha blending over operation in linear color space.
+pub fn rgba_alpha_blending_linear_over(a: Rgba, b: Rgba) -> Rgba {
+    let alpha = a[3] + b[3] * (1.0 - a[3]);
+    [
+        a[0] * a[3] + b[0] * b[3] * (1.0 - a[3]),
+        a[1] * a[3] + b[1] * b[3] * (1.0 - a[3]),
+        a[2] * a[3] + b[2] * b[3] * (1.0 - a[3]),
+        alpha,
+    ]
+}
+
+/// Alpha blending over operation in sRGB color space.
+pub fn rgba_alpha_blending_srgb_over(a: Rgba, b: Rgba) -> Rgba {
+    let a = rgba_gamma_srgb_to_linear(a);
+    let b = rgba_gamma_srgb_to_linear(b);
+    rgba_gamma_linear_to_srgb(rgba_alpha_blending_linear_over(a, b))
+}
+
+/// Alpha blending over operation in sRGB color space with `u8` precision.
+pub fn rgba_alpha_blending_srgb_over_u8(a: Rgba<u8>, b: Rgba<u8>) -> Rgba<u8> {
+    rgba_to_u8(rgba_alpha_blending_srgb_over(rgba_to_f32(a), rgba_to_f32(b)))
+}
