9475a0dad3a6528d3b01e58040658364ad6f4d2a
Sven Nilsen <bvssvni@gmail.com>
2025-12-08 14:12:15 +0100

    Published Turbine 0.1.0
    - Use dual license MIT and Apache 2.0
    - Removed old code
    - Reexported libraries
    
    See https://github.com/PistonDevelopers/turbine/issues/179

--- src/render/types.rs
+++
@@ -1,8 +0,0 @@
-//! Types.
-
-use gfx_debug_draw;
-use gfx_device_gl;
-
-/// The type of debug renderer.
-pub type DebugRenderer =
-    gfx_debug_draw::DebugRenderer<gfx_device_gl::Resources, gfx_device_gl::Factory>;
